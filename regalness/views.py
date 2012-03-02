45afd11348b16eed3abca6538e9ed1f2578b95f2
Jill Judd <jilljudd@Jill-Judds-MacBook-Pro.local>
2012-03-02 00:02:28 -0800

    ajaxed credit cards

--- regalness/views.py
+++ regalness/views.py
@@ -8,21 +8,19 @@ import stripe
 
 def index(request, template_name='regalness/index.html'):
     context = {}
+    if settings.DEBUG:
+        context['key'] = settings.TEST_STRIPE_PUB_KEY
+        context['card'] = settings.TEST_CARD_NUM
     return render_to_response(template_name, context, context_instance=RequestContext(request))
 
 
 def order(request, template_name='regalness/order.html'):
     context = {}
-    context['key'] = settings.TEST_STRIPE_PUB_KEY
-    if settings.DEBUG:
-        context['card'] = settings.TEST_CARD_NUM
     return render_to_response(template_name, context, context_instance=RequestContext(request))
 
 def order_submit(request, token, template_name='regalness/thanks.html'):
     context = {}
-    print 'submission' 
     stripe.api_key = settings.TEST_STRIPE_SECRET_KEY
-
     # create the charge on Stripe's servers - this will charge the user's card
     charge = stripe.Charge.create(
         amount=1000, # amount in cents, again
