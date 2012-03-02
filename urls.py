49a2419e7a690254ac35ab65e0e2ba2880fdefc2
Jill Judd <jilljudd@Jill-Judds-MacBook-Pro.local>
2012-03-02 00:39:46 -0800

    added options

--- urls.py
+++ urls.py
@@ -1,7 +1,5 @@
 from django.conf.urls.defaults import patterns, include, url
 from django.conf import settings
-
-# Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
 
@@ -9,10 +7,6 @@ urlpatterns = patterns('',
      url(r'^$', 'regalness.views.index', name='home'),
      url(r'^order/$', 'regalness.views.order', name='order'),
      url(r'^s/(?P<token>[\w-]+)/$', 'regalness.views.order_submit', name='order_submit'),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
 )
