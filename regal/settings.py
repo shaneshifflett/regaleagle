f9aa797c70503b7d3de39661b828dba1bec0a993
Shane Shifflett <shifflett.shane@gmail.com>
2012-08-02 13:35:52 -0700

    get manage right, urls with namespacing

--- regal/settings.py
+++ regal/settings.py
@@ -60,7 +60,7 @@ USE_L10N = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = map_path('regal/public')
+MEDIA_ROOT = map_path('public')
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
@@ -71,7 +71,7 @@ MEDIA_URL = 'public'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = map_path('regal/public/static')
+STATIC_ROOT = map_path('public/static')
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -122,7 +122,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    map_path('regal/templates'),
+    map_path('templates'),
 )
 
 INSTALLED_APPS = (
--- regal/urls.py
+++ regal/urls.py
@@ -4,14 +4,14 @@ from django.conf import settings
 # admin.autodiscover()
 
 urlpatterns = patterns('',
-     url(r'^$', 'orders.views.index', name='home'),
-     url(r'^experimental/$', 'orders.views.experimental', name='experimental'),
-     url(r'^register/$', 'orders.views.register', name='register'),
-     url(r'^login/$', 'orders.views.login', name='login'),
-     url(r'^deets/$', 'orders.views.order_details', name='order_details'),
-     url(r'^contact/$', 'orders.views.contact', name='contact'),
-     url(r'^review/$', 'orders.views.review', name='review'),
-     url(r'^s/(?P<option>[\w-]+)/(?P<quantity>\d+)/(?P<token>[\w-]+)/$', 'orders.views.order_submit', name='order_submit'),
+     url(r'^$', 'regal.orders.views.index', name='home'),
+     url(r'^experimental/$', 'regal.orders.views.experimental', name='experimental'),
+     url(r'^register/$', 'regal.orders.views.register', name='register'),
+     url(r'^login/$', 'regal.orders.views.login', name='login'),
+     url(r'^deets/$', 'regal.orders.views.order_details', name='order_details'),
+     url(r'^contact/$', 'regal.orders.views.contact', name='contact'),
+     url(r'^review/$', 'regal.orders.views.review', name='review'),
+     url(r'^s/(?P<option>[\w-]+)/(?P<quantity>\d+)/(?P<token>[\w-]+)/$', 'regal.orders.views.order_submit', name='order_submit'),
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
 )
